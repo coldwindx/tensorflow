915778208bb53839c6c9f0bac81f5579523df6a0
coldwindx <15653913658@163.com>
2022-06-01 09:18:47 +0800

    ...

---
+++ iris_keras.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+from sklearn import datasets
+import numpy as np
+
+x_data = datasets.load_iris().data
+y_data = datasets.load_iris().target
+
+np.random.seed(116)
+np.random.shuffle(x_data)
+np.random.seed(116)
+np.random.shuffle(y_data)
+
+tf.random.set_seed(116)
+
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Dense(3, activation = 'softmax', kernel_regularizer = tf.keras.regularizers.l2())
+# ])
+
+class IrisModel(tf.keras.Model):
+    def __init__(self) -> None:
+        super(IrisModel, self).__init__()
+        self.d1 = tf.keras.layers.Dense(3, activation = 'softmax', 
+                                            kernel_regularizer = tf.keras.regularizers.l2())
+    def call(self, x):
+        y = self.d1(x)
+        return y
+
+model = IrisModel()
+
+model.compile(optimizer = tf.keras.optimizers.SGD(lr = 0.1),
+                loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = False),
+                metrics = ['sparse_categorical_accuracy'])
+
+model.fit(x_data, y_data, batch_size = 32, epochs = 500, validation_split = 0.2, validation_freq = 20)
+
+model.summary()
---
+++ test.py
@@ -0,0 +1,83 @@
+import matplotlib
+matplotlib.use('Agg')
+
+import tensorflow as tf
+from matplotlib import pyplot as plt
+from sklearn import datasets 
+import numpy as np
+
+
+x_data = datasets.load_iris().data
+y_data = datasets.load_iris().target
+
+np.random.seed(116)
+np.random.shuffle(x_data)
+np.random.seed(116)
+np.random.shuffle(y_data)
+
+tf.random.set_seed(116)
+
+x_train = x_data[:-30]
+y_train = y_data[:-30]
+x_test = x_data[-30:]
+y_test = y_data[-30:]
+
+x_train = tf.cast(x_train, dtype = tf.float32)
+x_test = tf.cast(x_test, dtype = tf.float32)
+
+train_db = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(32)
+test_db = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+
+w1 = tf.Variable(tf.random.truncated_normal([4,3], stddev = 0.1, seed = 1))
+b1 = tf.Variable(tf.random.truncated_normal([3], stddev = 0.1, seed = 1))
+
+
+lr = 0.1
+epoch = 500
+losses = []
+accuracies = []
+
+for epoch in range(epoch):
+    total_loss = 0
+    with tf.GradientTape() as tape:
+        for step, (x_train, y_train) in enumerate(train_db):
+            y = tf.matmul(x_train, w1) + b1
+            y = tf.nn.softmax(y)
+            y_ = tf.one_hot(y_train, depth = 3)
+            loss = tf.reduce_mean(tf.square(y - y_))
+            total_loss += loss.numpy()
+        grads = tape.gradient(loss, [w1, b1])
+        w1.assign_sub(lr * grads[0])
+        b1.assign_sub(lr * grads[1])
+    
+    losses.append(total_loss / 4)
+    print("Epoch: {}, Loss: {}".format(epoch, total_loss / 4))
+
+    total_correct, total_number = 0, 0
+    for x_test, y_test in test_db:
+        y = tf.matmul(x_test, w1) + b1
+        y = tf.nn.softmax(y)
+        pred = tf.argmax(y, axis = 1)
+        pred = tf.cast(pred, dtype = y_test.dtype)
+        correct = tf.cast(tf.equal(pred, y_test), dtype = tf.int32)
+        correct = tf.reduce_sum(correct)
+        total_correct += int(correct)
+        total_number += y_test.shape[0]
+
+    accuracies.append(total_correct / total_number)
+    print("Epoch: {}, accuracy: {}".format(epoch, total_correct / total_number))
+
+plt.title("Loss Function Curve")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.plot(losses, label = "$Loss$")
+plt.savefig("loss.png")
+plt.show()
+
+plt.title("Accuracy Curve")
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+plt.plot(accuracies, label = "$Accuracy$")
+plt.savefig("accuracy.png")
+plt.show()
+
